782c095e6cdd7615f8ce22963da193d601c75642
Rafał Meisel <rafal.n.meisel@gmail.com>
2023-03-29 16:51:48 -0400

    Fix - create Developer File and List Apk File

--- modules/directories.py
+++ modules/directories.py
@@ -35,13 +35,26 @@ def createResultFile(RESULT_FILE_PATH):
         os.chmod(RESULT_FILE_PATH, 0o766)
         print("Created file: " + RESULT_FILE_PATH)
 
-def createGooglePlayDeveloperProfileUrlsFile(GOOGLE_PLAY_DEVELOPERS_URLS_FILE_PATH):
+def createDeveloperProfileUrlsFile(DEVELOPERS_URLS_PROFILE_FILE):
 
-    if not os.path.exists(GOOGLE_PLAY_DEVELOPERS_URLS_FILE_PATH):
-        f = open(GOOGLE_PLAY_DEVELOPERS_URLS_FILE_PATH, "r")
-        os.chmod(GOOGLE_PLAY_DEVELOPERS_URLS_FILE_PATH, 0o766)
-        print("Created file: " + GOOGLE_PLAY_DEVELOPERS_URLS_FILE_PATH + " - Please provide the Developers Urls from Google Play in this file.")
+    if not os.path.exists(DEVELOPERS_URLS_PROFILE_FILE):
+        f = open(DEVELOPERS_URLS_PROFILE_FILE, "r")
+        os.chmod(DEVELOPERS_URLS_PROFILE_FILE, 0o766)
+        print("Created file: " + DEVELOPERS_URLS_PROFILE_FILE + " - Please provide the Developers Urls from Google Play in this file.")
 
+def createApplicationListFile(APPLICATION_LIST_FILE_PATH):
+
+    if not os.path.exists(APPLICATION_LIST_FILE_PATH):
+        f = open(APPLICATION_LIST_FILE_PATH, "r")
+        os.chmod(APPLICATION_LIST_FILE_PATH, 0o766)
+        print("Created file: " + APPLICATION_LIST_FILE_PATH + " - Please provide the list with Application Package Names in this file.")
+
+def createDeveloperProfileUrlsFile(DEVELOPERS_URLS_PROFILE_FILE):
+
+    if not os.path.exists(DEVELOPERS_URLS_PROFILE_FILE):
+        f = open(DEVELOPERS_URLS_PROFILE_FILE, "r")
+        os.chmod(DEVELOPERS_URLS_PROFILE_FILE, 0o766)
+        print("Created file: " + DEVELOPERS_URLS_PROFILE_FILE + " - Please provide the Developers Urls in this file.")
 
 def clearResultFile(RESULT_FILE_PATH):
 
@@ -55,8 +68,10 @@ def clearApplicationListFile(APPLICATION_LIST_FILE):
     resultFile.truncate(0)
     resultFile.close()
     
-def createDirectories(INPUT_DIRECTORY_PATH, INPUT_ANALYZED_DIRECTORY_PATH, OUTPUT_DIRECTORY_PATH, OUTPUT_ANALYZED_DIRECTORY_PATH, RESULT_FILE_PATH):
+def createDirectories(DEVELOPERS_URLS_PROFILE_FILE, APPLICATION_LIST_FILE_PATH, INPUT_DIRECTORY_PATH, INPUT_ANALYZED_DIRECTORY_PATH, OUTPUT_DIRECTORY_PATH, OUTPUT_ANALYZED_DIRECTORY_PATH, RESULT_FILE_PATH)
 
+    createDeveloperProfileUrlsFile(DEVELOPERS_URLS_PROFILE_FILE)
+    createApplicationListFile(APPLICATION_LIST_FILE_PATH)
     createInputDirectory(INPUT_DIRECTORY_PATH)
     createInputAnalyzedDirectory(INPUT_ANALYZED_DIRECTORY_PATH)
     createOutputDirectory(OUTPUT_DIRECTORY_PATH)
