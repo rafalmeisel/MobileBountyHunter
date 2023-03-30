e778ae93ae7a971720ae8f0adecbcb6f7ae788d7
Rafał Meisel <rafal.n.meisel@gmail.com>
2023-03-30 16:05:22 -0400

    Fix open result file when not found any db files

--- modules/androidTests/checksAndroidDatabase.py
+++ modules/androidTests/checksAndroidDatabase.py
@@ -9,14 +9,16 @@ def searchFilesWithSqliteExtensions(OUTPUT_DIRECTORY_PATH, APPLICATION_PACKAGE_N
     applicationDirectoryPathLib = pathlib.Path(applicationDirectoryPath)
     sqliteFilesList = list(applicationDirectoryPathLib.rglob("*.sqlite"))
     
+    resultFile = open(RESULT_FILE_PATH, "a")
+
     if(len(sqliteFilesList) > 0):
-        
-        resultFile = open(RESULT_FILE_PATH, "a")
 
         for sqliteFilePath in sqliteFilesList:
             print(APPLICATION_PACKAGE_NAME + "Sqlite database: " + colored(str(sqliteFilePath), 'red'))
             resultFile.write(APPLICATION_PACKAGE_NAME + ": Sqlite database: " + str(sqliteFilePath) + "\n")
+        
         resultFile.close()
+
     else:
         print(APPLICATION_PACKAGE_NAME + ": Sqlite database: " + colored("Not found", 'blue'))
         resultFile.write(APPLICATION_PACKAGE_NAME + ": Sqlite database: Not found" + "\n")
@@ -27,14 +29,16 @@ def searchFilesWithDbExtensions(OUTPUT_DIRECTORY_PATH, APPLICATION_PACKAGE_NAME,
     applicationDirectoryPathLib = pathlib.Path(applicationDirectoryPath)
     dbFilesList = list(applicationDirectoryPathLib.rglob("*.db"))
     
+    resultFile = open(RESULT_FILE_PATH, "a")
+
     if(len(dbFilesList) > 0):
-        
-        resultFile = open(RESULT_FILE_PATH, "a")
 
         for dbFilePath in dbFilesList:
             print(APPLICATION_PACKAGE_NAME + "DB database: " + colored(str(dbFilePath), 'red'))
             resultFile.write(APPLICATION_PACKAGE_NAME + ": DB database: " + str(dbFilePath) + "\n")
+        
         resultFile.close()
+
     else:
         print(APPLICATION_PACKAGE_NAME + ": DB database: " + colored("Not found", 'blue'))
         resultFile.write(APPLICATION_PACKAGE_NAME + ": DB database: Not found" + "\n")
