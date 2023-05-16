a2993d977c4e5fc6654acc9ca9e3931c92e2818f
Rafał Meisel <rafal.n.meisel@gmail.com>
2023-05-16 19:52:09 -0400

    Fixes decompiling

--- sourceCode/androidTests/checksJavaFiles.py
+++ sourceCode/androidTests/checksJavaFiles.py
@@ -2,21 +2,19 @@ import pathlib
 from sourceCode.reporting import report
 from sourceCode.reporting import reportEnums
 
-# Any file with extension .sqlite or .db
-
 def findJavaScriptEnabled(OUTPUT_DIRECTORY_PATH, APPLICATION_PACKAGE_NAME):
     
-    applicationDirectoryPath = OUTPUT_DIRECTORY_PATH + APPLICATION_PACKAGE_NAME + "/" + "sources" + "/"
+    applicationDirectoryPath = OUTPUT_DIRECTORY_PATH + APPLICATION_PACKAGE_NAME + "/" + "smali" + "/"
     applicationDirectoryPathLib = pathlib.Path(applicationDirectoryPath)
     print(applicationDirectoryPathLib)
-    javaFilesList = [f for f in applicationDirectoryPathLib.rglob("*.java") if f.is_file()]
+    javaFilesList = [f for f in applicationDirectoryPathLib.rglob("*.smali") if f.is_file()]
 
     if len(javaFilesList) > 0:
 
         for javaFilePath in javaFilesList:
             with open(javaFilePath, "r") as f:
                 content = f.read()
-                if "javascriptenabled(true)" in content.lower():
+                if "setjavascriptenabled" in content.lower():
                     report.reportStatusToVerifyWithoutTokenValue(OUTPUT_DIRECTORY_PATH, APPLICATION_PACKAGE_NAME, str(javaFilePath), "JavaScriptEnabled")
                     report.copyFileToDedicatedReportDirectory(str(javaFilePath), OUTPUT_DIRECTORY_PATH, APPLICATION_PACKAGE_NAME)
 
