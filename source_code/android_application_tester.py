fe72ce612c87115966819c927cd125f49ece978a
Rafał Meisel <rafal.n.meisel@gmail.com>
2023-06-21 18:17:10 -0400

    Fix Application Tester to manage new implemenation

--- source_code/android_application_tester.py
+++ source_code/android_application_tester.py
@@ -1,19 +1,39 @@
 from source_code.config_file_manager import get_android_output_directory_relative_path
-from source_code.config_file_manager import get_android_decompiling_tool
+
 from source_code.android_tests.check_android_manifest_allow_backup import run_check_android_manifest_allow_backup
 from source_code.android_tests.check_android_manifest_debuggable import run_check_android_manifest_debuggable
 from source_code.android_tests.check_android_manifest_exported import run_check_android_manifest_exported
-from source_code.android_tests.check_res_values_strings import *
-from source_code.android_tests.check_files_names import *
-from source_code.android_tests.check_smali_files import *
+
+from source_code.android_tests.check_files_extention_config import search_config_files_with_config_extensions
+from source_code.android_tests.check_files_extention_db import search_files_with_db_extensions
+from source_code.android_tests.check_files_extention_sqlite import search_files_with_sqlite_extensions
+
 from source_code.android_tests.check_report_file import *
-import xml.etree.ElementInclude as ET
+
+from source_code.android_tests.check_string_aws_bucket import run_check_string_aws_bucket
+from source_code.android_tests.check_string_aws_long_term_access_key import run_check_string_aws_long_term_access_key
+from source_code.android_tests.check_string_aws_secret_access_key import run_check_string_aws_secret_access_key
+from source_code.android_tests.check_string_aws_short_term_access_key import run_check_string_aws_short_term_access_key
+from source_code.android_tests.check_string_cloudinary import run_check_string_cloudinary
+from source_code.android_tests.check_string_google_api_key import run_check_string_google_api_key
+from source_code.android_tests.check_string_google_app_spot import run_check_string_google_app_spot
+from source_code.android_tests.check_string_google_firebaseio import run_check_string_google_firebaseio
+from source_code.android_tests.check_string_google_oauth_access_token import run_check_string_google_oauth_access_token
+from source_code.android_tests.check_string_google_oauth_authorization_code import run_check_string_google_oauth_authorization_code
+from source_code.android_tests.check_string_google_oauth_refresh_token import run_check_string_google_oauth_refresh_token
+from source_code.android_tests.check_string_google_user_content import run_check_string_google_user_content
+from source_code.android_tests.check_string_javascript_enabled import run_check_string_javascript_enabled
+from source_code.android_tests.check_string_push_io_application_identifier import run_check_string_push_io_application_identifier
+
+import xml.etree.ElementTree as ET
+
 
 import os
 from termcolor import colored
 
 android_manifest_relative_file_path = ""
 android_res_values_strings_relative_file_path = ""
+application_package_system = "Android"
 
 def run_tests_android_application(application_package_name):
     
@@ -34,36 +54,43 @@ def run_tests_android_application(application_package_name):
     print("Analyzing application: " + colored(application_package_name, 'cyan'))
     
     android_manifest_basename = os.path.basename(android_manifest_relative_file_path)
-    android_manifest_content = open(android_manifest_relative_file_path, "r").readlines()
-    android_manifest_content_xml = ET.ElementTree(ET.fromstring(android_manifest_content))
-    
+
+    with open(android_manifest_relative_file_path, "r") as file:
+        android_manifest_content = file.read()
+        android_manifest_content_xml = ET.ElementTree(ET.fromstring(android_manifest_content))
+
     android_values_strings_basename = os.path.basename(android_res_values_strings_relative_file_path)
-    android_values_strings_content = open(android_res_values_strings_relative_file_path, "r").readlines()
+
+    with open(android_res_values_strings_relative_file_path, "r") as file:
+        android_values_strings_content = file.read()
     
     run_check_android_manifest_allow_backup(application_package_system, application_package_name, android_manifest_basename, android_manifest_content_xml)
     run_check_android_manifest_debuggable(application_package_system, application_package_name, android_manifest_basename, android_manifest_content_xml)
     run_check_android_manifest_exported(application_package_system, application_package_name, android_manifest_basename, android_manifest_content_xml)
     
-    check_res_values_strings_cloudinary(application_package_system, application_package_name, android_values_strings_basename, android_manifest_content)
-    check_res_values_strings_aws_long_term_access_keys(application_package_system, application_package_name, android_values_strings_basename, android_values_strings_content)
-    check_res_values_strings_aws_short_term_access_keys(application_package_system, application_package_name, android_values_strings_basename, android_values_strings_content)
-    check_res_values_strings_aws_secret_access_key(application_package_system, application_package_name, android_values_strings_basename, android_values_strings_content)
-    check_res_values_strings_aws_bucket(application_package_system, application_package_name, android_values_strings_basename, android_values_strings_content)
-    check_res_values_strings_push_io_application_identifier(application_package_system, application_package_name, android_values_strings_basename, android_values_strings_content)
-    check_res_values_strings_firebase_url(application_package_system, application_package_name, android_values_strings_basename, android_values_strings_content)
-    check_res_values_strings_aws_bucket(application_package_system, application_package_name, android_values_strings_basename, android_values_strings_content)
-    check_res_values_strings_google_api_key(application_package_system, application_package_name, android_values_strings_basename, android_values_strings_content)
-    check_res_values_strings_google_cloud_platform_google_user_content(application_package_system, application_package_name, android_values_strings_basename, android_values_strings_content)
-    check_res_values_strings_google_oauth_access_token(application_package_system, application_package_name, android_values_strings_basename, android_values_strings_content)
-    check_res_values_strings_google_app_spot(application_package_system, application_package_name, android_values_strings_basename, android_values_strings_content)
-
-    search_files_with_sqlite_extensions(application_package_name)
-    search_files_with_db_extensions(application_package_name)
+    search_config_files_with_config_extensions(application_package_system, application_package_name)
+    search_files_with_db_extensions(application_package_system, application_package_name)
+    search_files_with_sqlite_extensions(application_package_system, application_package_name)
     
-    search_config_files_with_any_extensions(application_package_name)
+    check_exported_activity_with_java_script_enabled(application_package_system, application_package_name)
+    
+    run_check_string_aws_bucket(application_package_system, application_package_name, android_values_strings_basename, android_manifest_content)
+    run_check_string_aws_long_term_access_key(application_package_system, application_package_name, android_values_strings_basename, android_values_strings_content)
+    run_check_string_aws_secret_access_key(application_package_system, application_package_name, android_values_strings_basename, android_values_strings_content)
+    run_check_string_aws_short_term_access_key(application_package_system, application_package_name, android_values_strings_basename, android_values_strings_content)
+    run_check_string_cloudinary(application_package_system, application_package_name, android_values_strings_basename, android_values_strings_content)
+    run_check_string_google_api_key(application_package_system, application_package_name, android_values_strings_basename, android_values_strings_content)
+    run_check_string_google_app_spot(application_package_system, application_package_name, android_values_strings_basename, android_values_strings_content)
+    run_check_string_google_firebaseio(application_package_system, application_package_name, android_values_strings_basename, android_values_strings_content)
+    run_check_string_google_oauth_access_token(application_package_system, application_package_name, android_values_strings_basename, android_values_strings_content)
+    run_check_string_google_oauth_authorization_code(application_package_system, application_package_name, android_values_strings_basename, android_values_strings_content)
+    run_check_string_google_oauth_refresh_token(application_package_system, application_package_name, android_values_strings_basename, android_values_strings_content)
+    run_check_string_google_user_content(application_package_system, application_package_name, android_values_strings_basename, android_values_strings_content)
+    run_check_string_javascript_enabled(application_package_system, application_package_name)
+    run_check_string_push_io_application_identifier(application_package_system, application_package_name, android_values_strings_basename, android_values_strings_content)
 
-    check_java_script_enabled(application_package_name)
-    check_exported_activity_with_java_script_enabled(application_package_name)
+
+    
 
 def run_android_tests_in_android_output_directory():
     
