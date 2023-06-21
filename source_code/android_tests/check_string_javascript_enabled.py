0f14606d6a86d600c10403a10048ab217dfa0cff
Rafał Meisel <rafal.n.meisel@gmail.com>
2023-06-21 18:15:49 -0400

    Set flag to report only one Not Found JavaScript

--- source_code/android_tests/check_string_javascript_enabled.py
+++ source_code/android_tests/check_string_javascript_enabled.py
@@ -25,9 +25,10 @@ def check_string_javascript_enabled(file_content):
 
 def run_check_string_javascript_enabled(application_package_system, application_package_name):
 
+    javascript_enabled_found = False
+
     android_output_directory_relative_path = get_android_output_directory_relative_path()
     dedicated_mobile_bounty_hunter_report_directory_relative_path = get_dedicated_mobile_bounty_hunter_report_directory_relative_path()
-    android_application_dedicated_report_directory = pathlib.Path(android_output_directory_relative_path, application_package_name, dedicated_mobile_bounty_hunter_report_directory_relative_path, os.path.basename(smali_file_path))
     
     file_regex = ""
     android_application_smali_directory_path = ""
@@ -43,16 +44,20 @@ def run_check_string_javascript_enabled(application_package_system, application_
 
     if len(files_list) > 0:
         for file_path in files_list:
+            android_application_dedicated_report_directory = pathlib.Path(android_output_directory_relative_path, application_package_name, dedicated_mobile_bounty_hunter_report_directory_relative_path, os.path.basename(file_path))
+            
             file_name = os.path.basename(file_path)
 
-            with open(file_path, "r") as smali_file:
-                file_content = smali_file.read()
+            with open(file_path, "r") as file:
+                file_content = file.read()
 
             javascript_enabled_items_list = check_string_javascript_enabled(file_content)
             
             if len(javascript_enabled_items_list) > 0:
+                javascript_enabled_found = True
                 for javascript_enabled_item in javascript_enabled_items_list:
                     report_issue(application_package_system, application_package_name, file_name, IssueSeverity.MEDIUM, IssueStatus.TO_VERIFY, "JavaScriptEnabled", javascript_enabled_item)
                     copy_file_to_dedicated_report_directory(str(file_path), str(android_application_dedicated_report_directory))
-            else:
-                report_issue(application_package_system, application_package_name, file_name, IssueSeverity.INFORMATIVE, IssueStatus.NOT_FOUND, "JavaScriptEnabled", "")
+    
+    if not (javascript_enabled_found):
+        report_issue(application_package_system, application_package_name, file_name, IssueSeverity.INFORMATIVE, IssueStatus.NOT_FOUND, "JavaScriptEnabled", "")
