fa7d11aac7685f3191ec8a8e5caaff67ccdcd16c
Rafał Meisel <rafal.n.meisel@gmail.com>
2023-06-21 13:00:11 -0400

    Added Google Authorization Code Unit Tests

---
+++ source_code/android_tests/check_string_google_authorization_code.py
@@ -0,0 +1,34 @@
+# These 2 lines are needed to perform Unit Tests.
+import sys
+sys.path.append('./')
+
+import re
+from source_code.report_manager import report_issue
+from source_code.report_manager import IssueSeverity
+from source_code.report_manager import IssueStatus
+
+# [2023.06.21] Reference: https://stackoverflow.com/questions/10291696/does-my-google-oauth2-token-look-right
+# [2023.06.21] Reference: https://developers.google.com/identity/protocols/oauth2/web-server?hl=pl
+# Valid Google Authorization Code: 4/Omoy
+# Valid Google Authorization Code: 4/P7q-oMsCeLvIaQm6bTrgtp7
+# Valid Google Authorization Code: 4/P7q7W91a-oMsCesrqpnmolkjsrqpnm7W91agASJKL12
+
+def check_string_google_authorization_code(file_content):
+    
+    google_authorization_code_regex = '4\/[0-9A-Za-z\-_]+'
+    google_authorization_code_items_list = []
+
+    google_authorization_code_items_list = re.findall(google_authorization_code_regex, file_content)
+    
+    return google_authorization_code_items_list
+
+
+def run_check_string_google_authorization_code(application_package_system, application_package_name, file_name, file_content):
+
+    google_authorization_code_items_list = check_string_google_authorization_code(file_content)
+    
+    if len(google_authorization_code_items_list) > 0:
+        for google_authorization_code_item in google_authorization_code_items_list:
+            report_issue(application_package_system, application_package_name, file_name, IssueSeverity.MEDIUM, IssueStatus.VULNERABLE, "Google Authorization Code", google_authorization_code_item)
+    else:
+        report_issue(application_package_system, application_package_name, file_name, IssueSeverity.INFORMATIVE, IssueStatus.NOT_FOUND, "Google Authorization Code", "")
