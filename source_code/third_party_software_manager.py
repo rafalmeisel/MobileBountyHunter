5cc69b5cdd17e412326dffd6c86db706884798cc
Rafał Meisel <rafal.n.meisel@gmail.com>
2023-06-26 16:29:29 -0400

    Change all invokes to subprocess

--- source_code/third_party_software_manager.py
+++ source_code/third_party_software_manager.py
@@ -23,47 +23,52 @@ def install_android_decompiler_apktools():
     
     if not os.path.exists(apktool_absolute_path):
         print("=== Installing ApkTools ===")
-
-        current_installation_step = 0
-        total_installation_steps = 6
         
-        current_installation_step+=1
-        print("Step " + str(current_installation_step) + "/" + str(total_installation_steps) + ": Download Linux wrapper script")
-        urllib.request.urlretrieve("https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/linux/apktool", "apktool.sh")
+        subprocess.run(['sudo', 'apt', 'install', 'apktool', '-y'])
 
-        current_installation_step+=1
-        print("Step " + str(current_installation_step) + "/" + str(total_installation_steps) + ": Download apktool-2")
-        urllib.request.urlretrieve("https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.7.0.jar", "apktool_2.7.0.jar")
-
-        current_installation_step+=1
-        print("Step " + str(current_installation_step) + "/" + str(total_installation_steps) + ": Rename downloaded jar to apktool.jar")
-        os.rename("apktool_2.7.0.jar", "apktool.jar")
-
-        current_installation_step+=1
-        print("Step " + str(current_installation_step) + "/" + str(total_installation_steps) + ": Move 'apktool.jar' and 'apktool' to /usr/local/bin")
-        shutil.move("apktool.sh" "/usr/local/bin/apktool.sh")
-        shutil.move("apktool.jar" "/usr/local/bin/apktool.jar")
-
-        current_installation_step+=1
-        print("Step " + str(current_installation_step) + "/" + str(total_installation_steps) + ": Make 'apktool.jar' and 'apktool' executable (755)")
-        os.chmod("/usr/local/bin/apktool.sh", 755)
-        os.chmod("/usr/local/bin/apktool.jar", 755)
+        # current_installation_step+=1
+        # print("Step " + str(current_installation_step) + "/" + str(total_installation_steps) + ": Install apktool")
         
-        current_installation_step+=1
-        print("Step " + str(current_installation_step) + "/" + str(total_installation_steps) + ": Check if apktool was successfully installed")
-        os.system("which apktool")
-    
+        # current_installation_step = 0
+        # total_installation_steps = 6
+        
+        # current_installation_step+=1
+        # print("Step " + str(current_installation_step) + "/" + str(total_installation_steps) + ": Download Linux wrapper script")
+        # urllib.request.urlretrieve("https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/linux/apktool", "apktool.sh")
+
+        # current_installation_step+=1
+        # print("Step " + str(current_installation_step) + "/" + str(total_installation_steps) + ": Download apktool-2")
+        # urllib.request.urlretrieve("https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.7.0.jar", "apktool_2.7.0.jar")
+
+        # current_installation_step+=1
+        # print("Step " + str(current_installation_step) + "/" + str(total_installation_steps) + ": Rename downloaded jar to apktool.jar")
+        # subprocess.run(['sudo', 'rename', 'apktool_2.7.0.jar', 'apktool.jar'])
+
+        # current_installation_step+=1
+        # print("Step " + str(current_installation_step) + "/" + str(total_installation_steps) + ": Move 'apktool.jar' and 'apktool' to /usr/local/bin")
+        # subprocess.run(['sudo', 'mv', 'apktool.sh', '/usr/local/bin/apktool.sh'])
+        # subprocess.run(['sudo', 'mv', 'apktool.jar', '/usr/local/bin/apktool.jar'])
+
+        # current_installation_step+=1
+        # print("Step " + str(current_installation_step) + "/" + str(total_installation_steps) + ": Make 'apktool.jar' and 'apktool' executable (755)")
+        # subprocess.run(['sudo', 'chmod', '775', '/usr/local/bin/apktool.sh'])
+        # subprocess.run(['sudo', 'chmod', '775', '/usr/local/bin/apktool.jar'])
+
+        # current_installation_step+=1
+        # print("Step " + str(current_installation_step) + "/" + str(total_installation_steps) + ": Check if apktool was successfully installed")
+        # subprocess.run(['which', 'apktool'])
 
 # Tool to download Android application from Google Play
 def install_apkeep():
     
-    apkeep_absolute_path = '/home/kali/.cargo/bin/apkeep'
+    apkeep_absolute_path = '$HOME/cargo/bin/apkeep'
 
     if not os.path.exists(apkeep_absolute_path):
         print("=== Installing Apkeep ===")
-        os.system("apt install libssl-dev")
-        os.system("apt install pkg-config -y")
-        os.system("cargo install apkeep --locked")
+        subprocess.run(['sudo', 'apt', 'install', 'libssl-dev', '-y'])
+        subprocess.run(['sudo', 'apt', 'install', 'pkg-config', '-y'])
+        subprocess.run(['sudo', 'cargo', 'install', 'cargo', '-y'])
+        subprocess.run(['sudo', 'cargo', 'install', 'apkeep', '--locked'])
 
 
 # Tool to using AWS Console
@@ -73,7 +78,7 @@ def install_aws_cli():
 
     if not os.path.exists(aws_cli_absolute_path):
         print("=== Installing AwsCli ===")
-        os.system("apt install awscli -y")
+        subprocess.run(['sudo', 'apt', 'install', 'awscli', '-y'])
 
 
 def install_third_party_software():
