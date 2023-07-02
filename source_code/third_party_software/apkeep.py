8be5573b3a339e9098725b1bd82a0ad925b3913b
Rafał Meisel <rafal.n.meisel@gmail.com>
2023-07-02 08:37:56 -0400

    Add checks  apkeep installed but not added to path

--- source_code/third_party_software/apkeep.py
+++ source_code/third_party_software/apkeep.py
@@ -1,5 +1,6 @@
 import subprocess
 import distro
+import getpass
 import os
 from source_code.config_file_manager import get_autoinstall_third_party_software
 
@@ -19,12 +20,12 @@ def show_installation_process_android_decompiler_apkeep():
     distribution = distro.id()
 
     if (distribution == "kali" or distribution =="debian" or distribution == "linuxmint"):
-        print("Please install apkeep using commands:")
-        print("sudo apt install libssl-dev -y")
-        print("sudo apt install pkg-config -y")
-        print("sudo apt install cargo -y")
-        print("sudo cargo install apkeep --locked")
-        print("export PATH=$PATH:/root/.cargo/bin")
+        print('Please install apkeep using commands:')
+        print('apt install libssl-dev -y')
+        print('apt install pkg-config -y')
+        print('apt install cargo -y')
+        print('cargo install apkeep --locked')
+        print('export PATH="$PATH:/home/' + getpass.getuser() + '/.cargo/bin"')
 
     else:
         print("You're using " + distribution + " system. It suprised us that you are using this system. Please, let us know about adding this system to the script ;)")
@@ -35,14 +36,11 @@ def install_android_apkeep():
 
     print("=== Installing Apkeep ===")
     if (distribution == "kali" or distribution =="debian"):
-        subprocess.run('sudo apt install libssl-dev -y', shell=True, check=True)
-        subprocess.run('sudo apt install pkg-config -y', shell=True, check=True)
-        subprocess.run('sudo apt install cargo -y', shell=True, check=True)
-        subprocess.run('sudo cargo install apkeep --locked', shell=True, check=True)
-
-        path = os.environ.get('PATH', '')
-        new_path = '/root/.cargo/bin:' + path
-        os.environ['PATH'] = new_path
+        subprocess.run('apt install libssl-dev -y', shell=True, check=True)
+        subprocess.run('apt install pkg-config -y', shell=True, check=True)
+        subprocess.run('apt install cargo -y', shell=True, check=True)
+        subprocess.run('cargo install apkeep --locked', shell=True, check=True)
+        subprocess.run('export PATH=$PATH:/home/' + getpass.getuser() + '/.cargo/bin', shell=True, check=True)
 
     elif (distribution == "linuxmint"):
         subprocess.run('flatpak install flathub com.github.skylot.jadx', shell=True, check=True)
@@ -51,11 +49,26 @@ def install_android_apkeep():
         print("You're using " + distribution + " system. It suprised us that you are using this system. Please, let us know about adding this system to the script ;)")
 
 
+# In Linux Mint, there is an issue to update $PATH with ./cargo/bin
+# Due to this issue each time, when user run the script, there is check:
+# 1. to verify if apkeep is already installed:
+# a) yes - add /.cargo/bin to $PATH
+# b) no - continue script and install apkeep
+def check_apkeep_and_update_path():
+    
+    is_appkeep_installed = os.path.isfile('/home/' + getpass.getuser() + '/.cargo/bin/apkeep')
+    if (is_appkeep_installed):
+        subprocess.run('export PATH=$PATH:/home/' + getpass.getuser() + '/.cargo/bin', shell=True, check=True)
+    
+
 def run_install_process_apkeep():
     
     is_autoinstall_third_party_software = get_autoinstall_third_party_software()
+    
+    check_apkeep_and_update_path()
 
     if not (is_installed_apkeep()):
+
         if (is_autoinstall_third_party_software):
             install_android_apkeep()
         else:
